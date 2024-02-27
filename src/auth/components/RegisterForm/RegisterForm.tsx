8ebc66b5a82fb152e71976f6b572621533a9eb90
Mario González <mario@mariogl.com>
2024-02-27 10:55:47 +0100

    Demo 5

--- src/auth/components/RegisterForm/RegisterForm.tsx
+++ src/auth/components/RegisterForm/RegisterForm.tsx
@@ -1,41 +1,47 @@
-import { useNavigate } from "react-router-dom";
 import Form from "@/forms/components/Form";
 import FormGroup from "@/forms/components/FormGroup";
 import {
-  TextField,
-  PasswordField,
   CheckBoxField,
+  PasswordField,
+  TextField,
 } from "@/forms/components/InputField";
 import Button from "@/shared/components/Button";
 
-const LoginForm = (): React.ReactElement => {
-  const labelElement = <>Remember me</>;
-  const navigate = useNavigate();
-
-  const login = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    navigate("/menu");
-  };
+const RegisterForm = (): React.ReactElement => {
+  const labelElement = (
+    <>
+      I accept the <a href="">Privacy Policy</a>
+    </>
+  );
 
   return (
     <>
-      <p>Please enter your credentials</p>
-      <Form onSubmit={login}>
+      <p>Please enter your data</p>
+      <Form>
+        <FormGroup>
+          <TextField id="name" labelText="Your name" />
+        </FormGroup>
         <FormGroup>
           <TextField id="username" labelText="Your username" />
         </FormGroup>
         <FormGroup>
           <PasswordField id="password" labelText="Your password" />
         </FormGroup>
+        <FormGroup>
+          <PasswordField
+            id="repeat-password"
+            labelText="Confirm your password"
+          />
+        </FormGroup>
         <FormGroup variant="inline">
           <CheckBoxField id="privacy" labelText={labelElement} />
         </FormGroup>
         <FormGroup>
-          <Button variant="outline">Log in</Button>
+          <Button variant="outline">Sign up</Button>
         </FormGroup>
       </Form>
     </>
   );
 };
 
-export default LoginForm;
+export default RegisterForm;
